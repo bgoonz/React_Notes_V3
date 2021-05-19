5759372d2c85ef508c735ec4c3a962c0ac87d979
Bryan C Guner <bryan.guner@gmail.com>
2021-05-19 17:37:45 +0000

    merge

---
+++ my-other-notes-n-projects/REACT/REPOS/reactjs.org-master/gatsby-node.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * @emails react-core
+ */
+
+'use strict';
+
+exports.onCreateWebpackConfig = require('./gatsby/onCreateWebpackConfig');
+exports.createPages = require('./gatsby/createPages');
+exports.onCreateNode = require('./gatsby/onCreateNode');
+exports.onCreatePage = require('./gatsby/onCreatePage');
